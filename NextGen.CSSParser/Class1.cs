c6b152cf1e1e5b23d9f3b9b62e6ee2bba9a92f05
Sander van t Einde <sandervanteinde@gmail.com>
2017-12-08 09:23:16 +0100

    init solution

---
+++ NextGen.CSSParser/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGen.CSSParser
+{
+    public class Class1
+    {
+    }
+}
---
+++ NextGen.HTMLParser/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGen.HTMLParser
+{
+    public class Class1
+    {
+    }
+}
