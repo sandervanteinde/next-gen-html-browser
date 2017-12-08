45e15d1e406703a99660b5bb81402ead87a635a3
Sander van t Einde <sandervanteinde@gmail.com>
2017-12-08 09:28:28 +0100

    woopsie

---
+++ NextGen.HTMLParser/HTMLDocument.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGen.HTMLParser
+{
+    public class HTMLDocument
+    {
+        private string fileName;
+        public HTMLDocument(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void Parse()
+        {
+
+        }
+    }
+}
